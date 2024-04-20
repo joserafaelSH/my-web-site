c7cbcf7924a2e2509a1bac19d1c3c7c8451a9299
joserafaelSH <jrhermoso18@gmail.com>
2024-04-20 03:11:12 -0300

    a

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+
   theme: {
     extend: {},
   },
   plugins: [],
   darkMode: "selector",
-  site: "https://joserafaelSH.github.io",
+  site: "https://joserafaelsh.github.io",
   base: "my-web-site",
 };
