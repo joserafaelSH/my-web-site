0620b2edaac0ebe7e72c82d8733dbd6d8f91492c
joserafaelSH <jrhermoso18@gmail.com>
2024-04-23 21:32:26 -0300

    the end

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "dracula",
+      },
+
+      gfm: false,
+    }),
+  ],
   site: "https://joserafaelsh.github.io",
-  base: "my-web-site",
+  base: "blog",
 });
---
+++ src/content/blog/post-1.md
@@ -0,0 +1,6 @@
+---
+title: "Some thing"
+pubDate: 2024-04
+description: "In progress ..."
+author: "José Rafael"
+---
